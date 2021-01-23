e2a9061879acbc7d1b21d9ed85e7351a4c074c7f
Zulhardiyanto Hardi <zulhardiyanto.achmad@bts.id>
2021-01-23 20:59:46 +0700

    todo list added index route

--- app.js
+++ app.js
@@ -2,19 +2,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'todo_list_project',
+    password: 'postgres',
+    port: 5432
+})
+
+var indexRouter = require('./routes/index')(pool);
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors())
+app.use(bodyParser.json({ limit: '500mb'}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,114 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+
+module.exports = function (pool) {
+  /* GET home page. */
+  router.get('/index', function (req, res, next) {
+    pool.query('SELECT * FROM list_belanja', (err, results) => {
+      if (err) {
+        let message = {
+          message: "error",
+          error: true,
+          data: err
+        }
+        res.status(200).json(message)
+      } else {
+        let message = {
+          message: "success",
+          error: false,
+          data: results.rows
+        }
+        res.status(200).json(message)
+      }
+    })
+  });
+
+  router.get('/index/:id', function (req, res, next) {
+    const { id } = req.params
+    pool.query(`SELECT * FROM list_belanja WHERE id=${id}`, (err, results) => {
+      if (err) {
+        let message = {
+          message: "error",
+          error: true,
+          data: err
+        }
+        res.status(200).json(message)
+      } else {
+        let message = {
+          message: "success",
+          error: false,
+          data: results.rows[0]
+        }
+        res.status(200).json(message)
+      }
+    })
+  });
+
+  router.put('/index/:id', function (req, res, next) {
+    const { id } = req.params
+    const { nama_barang } = req.body
+    pool.query(`UPDATE list_belanja SET nama_barang='${nama_barang}' WHERE id=${id}`, (err, results) => {
+      if (err) {
+        let message = {
+          message: "error",
+          error: true,
+          data: err
+        }
+        res.status(200).json(message)
+      } else {
+        let message = {
+          message: "success",
+          error: false,
+          data: results.rows[0]
+        }
+        res.status(200).json(message)
+      }
+    })
+  });
+
+  router.delete('/index/:id', function (req, res, next) {
+    const { id } = req.params
+    pool.query(`DELETE FROM list_belanja WHERE id=${id}`, (err, results) => {
+      if (err) {
+        let message = {
+          message: "error",
+          error: true,
+          data: err
+        }
+        res.status(200).json(message)
+      } else {
+        let message = {
+          message: "success",
+          error: false,
+          data: results.rows[0]
+        }
+        res.status(200).json(message)
+      }
+    })
+  });
+
+  router.post('/index', function (req, res, next) {
+    const { nama_barang } = req.body
+    pool.query(`INSERT INTO list_belanja(nama_barang) VALUES ('${nama_barang}')`, (err, results) => {
+      if (err) {
+        let message = {
+          message: "error",
+          error: true,
+          data: err
+        }
+        res.status(200).json(message)
+      } else {
+        let message = {
+          message: "success",
+          error: false,
+          data: results.rows[0]
+        }
+        res.status(200).json(message)
+      }
+    })
+  });
+
+  return router
+}
